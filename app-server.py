177ab47294808ce6b6eced5723bf916e79eb793f
bazsomarci98 <bazsomarci98@gmail.com>
2022-04-29 21:12:15 +0200

    implement basic app-client and app-server based on the tutorial, leave working example in it for sample use

---
+++ app-server.py
@@ -0,0 +1,53 @@
+import selectors
+import socket
+import sys
+import traceback
+
+import libserver
+
+sel = selectors.DefaultSelector()
+port = 5150
+
+
+def accept_incoming_request(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    message = libserver.Message(sel, conn, addr)
+    sel.register(conn, selectors.EVENT_READ, data=message)
+
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <host>")
+    sys.exit(1)
+
+host, port = sys.argv[1], port
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)  # TODO: timeout value
+        for key, mask in events:
+            if key.data is None:
+                accept_incoming_request(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        f"Main: Error: Exception for {message.addr}:\n"
+                        f"{traceback.format_exc()}"
+                    )
+                    message.close()
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
